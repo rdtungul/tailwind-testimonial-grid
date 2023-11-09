5c8a2390f5f467feaa0dec1e5b9ab87091a69f80
rdtungul <rosephdarl@gmail.com>
2023-11-09 22:44:37 +0800

    testimonial grid setup

---
+++ js/index.js
@@ -0,0 +1,4 @@
+// automatically updated calendar year.
+// const year = new Date().getFullYear();
+// document.getElementsByClassName("auto-year")[0].textContent = `© ${year}`;
+// console.log(year);
